495f14c29232afb2c2a8b253608c86474ee0325e
404NameNotFound <lukas.striebel@hotmail.com>
2014-03-20 00:23:16 +0100

    Update Conviction.java

--- src/ch/ethz/inf/dbproject/model/Conviction.java
+++ src/ch/ethz/inf/dbproject/model/Conviction.java
@@ -1,5 +1,7 @@
 package ch.ethz.inf.dbproject.model;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Date;
 
 /**
@@ -7,33 +9,54 @@ import java.util.Date;
  */
 public class Conviction {
 
+	private final int idcon;
 	private final Date date;
-	private final Date endDate;
+	private final Date enddate;
 	private final String type;
-	private final Case caze;
+	private final int idcase;
+	private final int idpoi;
 
 	public Date getDate() {
 		return date;
 	}
 
-	public Date getEndDate() {
-		return endDate;
+	public Date getEnddate() {
+		return enddate;
 	}
 
 	public String getType() {
 		return type;
 	}
 	
-	public String getCaze() {
-		return caze.getTitle();
+	public int getIdcase() {
+		return idcase;
 	}
 
 	
-	public Conviction(final Date date, final Date endDate, final String type, final Case caze) {
+	public int getIdcon() {
+		return idcon;
+	}
+
+	public int getIdpoi() {
+		return idpoi;
+	}
+
+	public Conviction(final int idcon, final Date date, final Date enddate, final String type, final int idcase, final int idpoi) {
+		this.idcon = idcon;
 		this.date = date;
-		this.endDate = endDate;
+		this.enddate = enddate;
 		this.type = type;
-		this.caze = caze;
+		this.idcase = idcase;
+		this.idpoi = idpoi;
+	}
+	
+	public Conviction(final ResultSet rs) throws SQLException {
+		this.idcon = rs.getInt("idconviction");
+		this.date = rs.getDate("begindate");
+		this.enddate = rs.getDate("enddate");
+		this.type = rs.getString("type");
+		this.idcase = rs.getInt("idcase");
+		this.idpoi = rs.getInt("idpersonofinterest");
 	}
 
 		
