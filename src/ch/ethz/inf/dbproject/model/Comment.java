ffa176936348a5c562e933a1bd6791d25422fa89
404NameNotFound <lukas.striebel@hotmail.com>
2014-03-20 14:58:10 +0100

    Update Comment.java

--- src/ch/ethz/inf/dbproject/model/Comment.java
+++ src/ch/ethz/inf/dbproject/model/Comment.java
@@ -1,5 +1,8 @@
 package ch.ethz.inf.dbproject.model;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * Object that represents a user comment.
  */
@@ -7,10 +10,18 @@ public class Comment {
 
 	private final String username;
 	private final String comment;
+	private final int idnote;
 	
-	public Comment(final String username, final String comment) {
+	public Comment(final String username, final String comment, final int idnote) {
 		this.username = username;
 		this.comment = comment;
+		this.idnote = idnote;
+	}
+	
+	public Comment(final ResultSet rs) throws SQLException{
+		this.username = "not working yet";//rs.getString("username");
+		this.comment = rs.getString("text");
+		this.idnote = rs.getInt("nr");
 	}
 
 	public String getUsername() {
@@ -20,4 +31,8 @@ public class Comment {
 	public String getComment() {
 		return comment;
 	}	
+	
+	public int getIdnote() {
+		return idnote;
+	}
 }
