7165d6e8bdf422da0bb2351b97e32faeaa997b28
404NameNotFound <lukas.striebel@hotmail.com>
2014-03-20 14:50:39 +0100

    Update DatabaseInitiation.sql

--- DatabaseInitiation.sql
+++ DatabaseInitiation.sql
@@ -8,8 +8,12 @@ Insert into category(CatName, SuperCat) values ('Fraud', 'Property Crime');
 Insert into personofinterest(FirstName, LastName, DateOfBirth) values ('Andre', 'Rubbia', '1950-12-31');
 Insert into personofinterest(FirstName, LastName, DateOfBirth) values ('Jörg', 'Gutknecht', '1921-4-13');
 Insert into personofinterest(FirstName, LastName, DateOfBirth) values ('Bertrand', 'Meyer', '1921-4-14');
+Insert into personofinterest(FirstName, LastName, DateOfBirth) values ('Frank', 'Gürkaynak', '1967-9-2');
+Insert into personofinterest(FirstName, LastName, DateOfBirth) values ('Emo', 'Welzl', '1971-3-25');
 
+Insert into noteperson(idPersonOfInterest,text) values ('1', 'Sehr kräftig gebaut"');
 Insert into noteperson(idPersonOfInterest,text) values ('2', 'Höchstgefährlich');
+Insert into noteperson(idPersonOfInterest,text) values ('3', 'Auch bekannt als "Der Witwenmacher"');
 
 Insert into cases(Title,Description,open,date,time,location,CatName) values ('Entwendung einer Schreibmaschine', 'Auf einmal war sie weg...', false, '2010-10-30', '13:45:00', 'Rämistrasse 101', 'Theft');
 Insert into cases(Title,Description,open,date,time,location,CatName) values ('Mord an der ETH', 'Brutal mit Schreibmaschine erschlagen', true, '2010-10-31', '23:55:00', 'Rämistrasse 101', 'Murder');
