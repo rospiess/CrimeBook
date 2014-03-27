a26caa3b829ae3a76841d432d3b97b84f61c4426
FR4NK-W <wirzf@student.ethz.ch>
2014-03-28 00:06:21 +0100

    DatastoreInterface preparedStatement Username in deleteNote

--- src/ch/ethz/inf/dbproject/model/DatastoreInterface.java
+++ src/ch/ethz/inf/dbproject/model/DatastoreInterface.java
@@ -205,12 +205,13 @@ public final class DatastoreInterface {
 
 	}
 	
-	public final void deleteNote(int Nr) {
+	public final void deleteNote(int Nr, String username) {
 		// Deletes a caseNote with a certain Nr as key
 		try {
 			PreparedStatement s = sqlConnection
-					.prepareStatement("delete from notecase where Nr = ?");
+					.prepareStatement("delete from notecase where Nr = ? and username = ?");
 			s.setString(1, ""+Nr+"");
+			s.setString(2, ""+username+"");
 			s.execute();
 			s.close();
 
