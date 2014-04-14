533e34ce7dea1beb2d5f4e22bc4c6b81e8a9f304
FR4NK-W <wirzf@student.ethz.ch>
2014-04-14 23:54:15 +0200

    Legibility of init code when code wraps on console Success/error message visibility

--- DatabaseInitiationUTF8.sql
+++ DatabaseInitiationUTF8.sql
@@ -1,7 +1,7 @@
 USE crimedatabase;
 
-insert into user(username, password) values('Max Muster', '12345');
-insert into user(username, password) values('Test', '1');
+Insert into user(username, password) values('Max Muster', '12345');
+Insert into user(username, password) values('Test', '1');
 
 Insert into category(CatName) values ('Personal Crime');
 Insert into category(CatName, SuperCat) values ('Assault', 'Personal Crime');
@@ -29,23 +29,36 @@ Insert into noteperson(idPersonOfInterest,text,username) values ('1', 'Sehr krä
 Insert into noteperson(idPersonOfInterest,text,username) values ('1', 'Besitzt eine Vorliebe für Fastfood','Max Muster');
 Insert into noteperson(idPersonOfInterest,text,username) values ('2', 'Höchstgefährlich','Max Muster');
 Insert into noteperson(idPersonOfInterest,text,username) values ('3', 'Auch bekannt als "Der Witwenmacher"','Max Muster');
+
 insert into address(country, zipCode, city, street, streetNo) values ('Schweiz',8000,'Zürich','Rämistrasse',101);
 insert into address(country, zipCode, city, street, streetNo) values ('Schweiz',8000,'Zürich','Universitätsstrasse',6);
 insert into address(country, zipCode, city, street, streetNo) values ('Schweiz',8000,'Zürich','',-1);
 insert into address(country, zipCode, city, street, streetNo) values ('Frankreich',85113,'Port-Joinville','',-1);
 
 Insert into cases(Title,Description,open,date,time,CatName,idAddress,username) values ('Entwendung einer Schreibmaschine', 'Auf einmal war sie weg...', false, '2010-10-30', '13:45:00',  'Theft', 1,'Test');
+
 Insert into cases(Title,Description,open,date,time,CatName,idAddress,username) values ('Mord an der ETH', 'Brutal mit Schreibmaschine erschlagen', true, '2010-10-31', '23:55:00', 'Murder', 1,'Test');
+
 Insert into cases(Title,Description,open,date,time,CatName,idAddress,username) values ('Älterer Mann überfahren', 'Ein renommierter Professor wurde von einem Auto mit französischem Kennzeichen angefahren, welches anschliessend Fahrerflucht beging', true, '2010-11-01', '09:32:13', 'Assault', 1,'Test');
+
 Insert into cases(Title,Description,open,date,time,CatName,idAddress,username) values ('Einbruch ins CAB', 'Entwendet wurden: 9 Flaschen Bier, 2 Tiefkühlpizzas und eine nicht funktionierende Kaffeemaschine', true, '2014-3-20', '00:48:19', 'Burglary', 2,'Test');
+
 Insert into cases(Title,Description,open,date,time,CatName,idAddress,username) values ('Entführung des ETH Maskottchens', 'Opfer wurde mit Elektroschocker ausser Gefecht gestetzt und anschliessend in schwarzen Van verfrachtet', false, '2013-2-28', '21:34:51',  'Kidnapping', 1,'Test');
+
 Insert into cases(Title,Description,open,date,time,CatName,idAddress,username) values ('Bösartige Turingmaschine', 'Transferierte das Budget des VIS auf ausländisches Konto', false, '2011-1-21', '15:12:34', 'Fraud', 2,'Test');
+
 Insert into cases(Title,Description,open,date,time,CatName,idAddress,username) values ('Daten Verkauf', 'ETH Daten an ausländischen Geheimdienst verkauft', true, null, null, 'OtherProp', 3,'Max Muster');
+
 Insert into cases(Title,Description,open,date,time,CatName,idAddress,username) values ('Mensa-Essen vergiftet','Alle Studenten bekamen Bauchweh.', true, '2012-10-11','12:00:00', 'OtherPers', 1, 'Max Muster');
+
 Insert into cases(Title,Description,open,date,time,CatName,idAddress,username) values ('Uranentwendung','Uran wurde aus dem ETH Forschungsreaktor entwendet', true, '2012-03-13','03:07:16', 'Burglary', 1, 'Test');
+
 Insert into cases(Title,Description,open,date,time,CatName,idAddress,username) values ('Tintin entführt','Auf der Insel Île d\'Yeu wurde Tintin entführt.', false, '2009-08-17','14:13:12', 'Kidnapping', 4, 'Test');
+
 Insert into cases(Title,Description,open,date,time,CatName,idAddress,username) values ('Kap Haddock Whiskydiebstahl', 'Kapitän Haddock hat seine Whiskyflasche abgestellt. Auf einmal war sie weg...', false, '2001-10-30', '13:45:00',  'Theft', 4,'Test');
+
 Insert into notecase(idCase,text,username) values ('2', 'Opfer war bekannt dafür, Witze über die Programmiersprache Eiffel zu reissen.','Max Muster');
+
 Insert into notecase(idCase,text,username) values (7, '"Alle Professoren sind mit einem ausländischen Geheimdienst unter einer Decke" - angebliches Zitat von D.K.','Max Muster');
 
 Insert into conviction(beginDate,endDate,idCase,idPersonOfInterest) values ('2010-11-2','2010-12-2',1,3);
